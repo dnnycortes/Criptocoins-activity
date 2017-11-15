caa8bce4e746c4ebaf273c125dff12bc7cdd9ddd
Yonatan A. Rojas Serrano <yonatanrojas09@gmail.com>
2017-11-14 21:11:16 -0600

    added variables

---
+++ app/app.js
@@ -0,0 +1,11 @@
+
+(function() {
+  'use strict';
+  var input = document.getElementById('input');
+  var divisa = document.getElementById('divisa');
+  var cryptomonedas = document.getElementById('cryptomonedas');
+  var output = document.getElementById('output');
+
+  
+	
+}());
